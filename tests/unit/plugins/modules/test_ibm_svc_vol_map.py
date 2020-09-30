143b0464c44d784264a84289ef3aeb97993305ad
Shilpi-J <65765546+Shilpi-J@users.noreply.github.com>
2020-09-30 17:24:44 +0530

    Generic Ansible Module support (#14)
    * Generic Ansible Module
    
    1. Generic Ansible Module
    2. Support for Thin-provisioned volume
    3. Support for change of fcwwpn of an existing host.
    
    * Added common module to support Generic Ansible Module
    
    * update README
    
    * Unit tests
    
    * Unit Test Files for the Modules
    
    * Unit Test Files for Common Modules
    
    * UT files for Common Modules
    
    * sample playbooks for generic ansible module
    
    * delete place_holder
    
    Co-authored-by: Rohit <rohitk.work@outlook.com>

--- tests/unit/plugins/modules/test_ibm_svc_vol_map.py
+++ tests/unit/plugins/modules/test_ibm_svc_vol_map.py
@@ -65,7 +65,7 @@ class TestIBMSVCvdiskhostmap(unittest.TestCase):
         self.addCleanup(self.mock_module_helper.stop)
         self.restapi = IBMSVCRestApi(self.mock_module_helper, '1.2.3.4',
                                      'domain.ibm.com', 'username', 'password',
-                                     False, '/tmp/test.log')
+                                     False, 'test.log')
 
     def set_default_args(self):
         return dict({
@@ -103,9 +103,10 @@ class TestIBMSVCvdiskhostmap(unittest.TestCase):
                         "mapping_type": "private", "host_cluster_id": "",
                         "host_cluster_name": "", "protocol": "scsi"}]
         svc_obj_info_mock.return_value = mapping_ret
-        host_mapping = IBMSVCvdiskhostmap().get_existing_vdiskhostmap()
-        self.assertEqual('volume_Ansible_collections', host_mapping['name'])
-        self.assertEqual('0', host_mapping['id'])
+        host_mapping_data = IBMSVCvdiskhostmap().get_existing_vdiskhostmap()
+        for host_mapping in host_mapping_data:
+            self.assertEqual('volume_Ansible_collections', host_mapping['name'])
+            self.assertEqual('0', host_mapping['id'])
 
     @patch('ansible_collections.ibm.spectrum_virtualize.plugins.modules.'
            'ibm_svc_vol_map.IBMSVCvdiskhostmap.get_existing_vdiskhostmap')
@@ -135,11 +136,12 @@ class TestIBMSVCvdiskhostmap(unittest.TestCase):
     @patch('ansible_collections.ibm.spectrum_virtualize.plugins.modules.'
            'ibm_svc_vol_map.IBMSVCvdiskhostmap.get_existing_vdiskhostmap')
     @patch('ansible_collections.ibm.spectrum_virtualize.plugins.modules.'
-           'ibm_svc_vol_map.IBMSVCvdiskhostmap.vdiskhostmap_probe')
+           'ibm_svc_vol_map.IBMSVCvdiskhostmap.vdiskhostmap_create')
     @patch('ansible_collections.ibm.spectrum_virtualize.plugins.module_utils.'
            'ibm_svc_utils.IBMSVCRestApi._svc_authorize')
     def test_create_host_mapping_but_host_mapping_existed(
-            self, svc_authorize_mock, host_mapping_probe_mock,
+            self, svc_authorize_mock,
+            vdiskhostmap_create_mock,
             get_existing_vdiskhostmap_mock):
         set_module_args({
             'clustername': 'clustername',
@@ -150,19 +152,14 @@ class TestIBMSVCvdiskhostmap(unittest.TestCase):
             'volname': 'volume0',
             'host': 'host4test',
         })
-        mapping_ret = [{"id": "0", "name": "volume_Ansible_collections",
-                        "SCSI_id": "0", "host_id": "14",
-                        "host_name": "host_ansible_collects",
-                        "vdisk_UID": "6005076810CA0166C00000000000019F",
-                        "IO_group_id": "0", "IO_group_name": "io_grp0",
-                        "mapping_type": "private", "host_cluster_id": "",
-                        "host_cluster_name": "", "protocol": "scsi"}]
-        get_existing_vdiskhostmap_mock.return_value = mapping_ret
-        host_mapping_probe_mock.return_value = []
+        host_mapping = {u'message': u'Virtual Disk to Host map, id [0], '
+                                    u'successfully created', u'id': u'0'}
+        vdiskhostmap_create_mock.return_value = host_mapping
+        get_existing_vdiskhostmap_mock.return_value = []
         host_mapping_created = IBMSVCvdiskhostmap()
         with pytest.raises(AnsibleExitJson) as exc:
             host_mapping_created.apply()
-        self.assertFalse(exc.value.args[0]['changed'])
+        self.assertTrue(exc.value.args[0]['changed'])
         get_existing_vdiskhostmap_mock.assert_called_with()
 
     @patch('ansible_collections.ibm.spectrum_virtualize.plugins.modules.'
