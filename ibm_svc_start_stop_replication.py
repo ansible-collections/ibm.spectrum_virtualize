052e86ba5243566bb7a27c6d7010146c6e41fcdc
Rohit <rohitk.work@outlook.com>
2020-12-18 14:56:56 +0530

    Add Support for Remote Copy

---
+++ ibm_svc_start_stop_replication.py
@@ -0,0 +1,355 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020 IBM CORPORATION
+# Author(s): Rohit Kumar <rohit.kumar6@ibm.com>
+#
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'metadata_version': '1.1'}
+
+DOCUMENTATION = '''
+---
+module: ibm_svc_start_stop_replication
+short_description: This module starts/stops remote copies on
+                   IBM Spectrum Virtualize Family storage systems.
+version_added: "2.10.0"
+
+description:
+  - Ansible interface to manage remote copy related commands.
+
+options:
+  name:
+    description:
+      - Specifies a name to assign to the new remote copy relationship/group
+        or to operate on the existing remote copy.
+    required: false
+    type: str
+  state:
+    description:
+      - starts (C(started)), stops (C(stopped)) a
+        remote copy relationship.
+    choices: [started, stopped]
+    required: true
+    type: str
+  clustername:
+    description:
+    - The hostname or management IP of the Spectrum Virtualize storage system.
+    type: str
+    required: true
+  domain:
+    description:
+    - Domain for the Spectrum Virtualize storage system.
+    type: str
+  username:
+    description:
+    - REST API username for the Spectrum Virtualize storage system.
+    required: true
+    type: str
+  password:
+    description:
+    - REST API password for the Spectrum Virtualize storage system.
+    required: true
+    type: str
+  primary:
+    description:
+    - Specifies the copy direction by defining which disk
+      becomes the primary (source).
+      Applies when state is 'started'.
+    type: str
+    choices: [ 'master', 'aux' ]
+  isgroup:
+    description:
+    - Specifies that a consistency group has to be started or stopped.
+    default: false
+    type: bool
+  clean:
+    description:
+    - Specifies that the volume that is to become a secondary is clean.
+      Applies when state is 'started'.
+    default: false
+    type: bool
+  access:
+    description:
+    - Instructs the system to allow write access to a consistent secondary volume.
+      Applies when state is 'stopped'.
+    default: false
+    type: bool
+  force:
+    description:
+    - Specifies that the system must process the copy operation even if it
+      causes a temporary loss of consistency during synchronization.
+      Applies when state is 'started'.
+    type: bool
+  validate_certs:
+    description:
+    - Validates certification.
+    default: false
+    type: bool
+  log_path:
+    description:
+    - Path of debug log file.
+    type: str
+author:
+    - rohit(@rohitk-github)
+'''
+
+EXAMPLES = '''
+- name: Using Spectrum Virtualize collection for data replication
+  hosts: localhost
+  gather_facts: no
+  vars:
+    clustername: x.x.x.x
+    username: username
+    password: password
+  collections:
+    - ibm.spectrum_virtualize
+  connection: local
+  tasks:
+    - name: Start remote copy with name sample_rcopy
+      ibm_svc_start_stop_replication:
+        name: sample_rcopy
+        clustername: "{{clustername}}"
+        username: "{{username}}"
+        password: "{{password}}"
+        log_path: /tmp/ansible.log
+        state: started
+        clean: true
+
+    - name: Stop remote copy
+      ibm_svc_start_stop_replication:
+        name: sample_rcopy
+        clustername: "{{clustername}}"
+        username: "{{username}}"
+        password: "{{password}}"
+        log_path: /tmp/ansible.log
+        state: stopped
+
+'''
+RETURN = '''
+'''
+
+
+from ansible.module_utils._text import to_native
+from ansible_collections.ibm.spectrum_virtualize.plugins.module_utils.ibm_svc_utils import IBMSVCRestApi, svc_argument_spec, get_logger
+from ansible.module_utils.basic import AnsibleModule
+from traceback import format_exc
+
+
+class IBMSVCStartStopReplication(object):
+    def __init__(self):
+        argument_spec = svc_argument_spec()
+
+        argument_spec.update(
+            dict(
+                name=dict(type='str'),
+                state=dict(type='str',
+                           required=True,
+                           choices=['started', 'stopped']),
+                force=dict(type='bool', required=False),
+                primary=dict(type='str', choices=['master', 'aux']),
+                clean=dict(type='bool', default=False),
+                access=dict(type='bool', default=False),
+                isgroup=dict(type='bool', default=False),
+            )
+        )
+
+        self.module = AnsibleModule(argument_spec=argument_spec,
+                                    supports_check_mode=True)
+
+        # logging setup
+        log_path = self.module.params['log_path']
+        log = get_logger(self.__class__.__name__, log_path)
+        self.log = log.info
+
+        # Required
+        self.name = self.module.params['name']
+        self.state = self.module.params['state']
+
+        # Optional
+        self.primary = self.module.params.get('primary', None)
+        self.clean = self.module.params.get('clean', False)
+        self.access = self.module.params.get('access', False)
+        self.force = self.module.params.get('force', False)
+        self.isgroup = self.module.params.get('isgroup', False)
+
+        self.restapi = IBMSVCRestApi(
+            module=self.module,
+            clustername=self.module.params['clustername'],
+            domain=self.module.params['domain'],
+            username=self.module.params['username'],
+            password=self.module.params['password'],
+            validate_certs=self.module.params['validate_certs'],
+            log_path=log_path
+        )
+
+    def existing_rc(self):
+        """
+        find the remote copy relationships such as Metro Mirror, Global Mirror
+        relationships visible to the system.
+
+        Returns:
+            None if no matching instances or a list including all the matching
+            instances
+        """
+        self.log('Trying to get the remote copy relationship %s', self.name)
+        data = self.restapi.svc_obj_info(cmd='lsrcrelationship',
+                                             cmdopts=None, cmdargs=[self.name])
+
+        return data
+
+    def existing_rccg(self):
+        """
+        find the remote copy consistency group visible to the system.
+
+        Returns:
+            None if no matching instances or a list including all the matching
+            instances
+        """
+        data_cg = {}
+        self.log('Trying to get the remote copy cg %s', self.name)
+        data = self.restapi.svc_obj_info(cmd='lsrcconsistgrp',
+                                             cmdopts=None, cmdargs=[self.name])
+        if isinstance(data, list):
+            data_cg = data[0]
+        elif isinstance(data, dict):
+            data_cg = data
+        return data_cg
+
+    def start(self):
+        """
+        Starts the Metro Mirror or Global Mirror relationship copy process, set
+        the direction of copy if undefined, and (optionally) mark the secondary
+        volume of the relationship as clean. The relationship must be a
+        stand-alone relationship.
+        """
+        cmdopts = {}
+        self.log("self.primary is %s", self.primary)
+        if self.primary:
+            cmdopts['primary'] = self.primary
+        if self.clean:
+            cmdopts['clean'] = self.clean
+        if self.force:
+            cmdopts['force'] = self.force
+        if self.isgroup:
+            result = self.restapi.svc_run_command(cmd='startrcconsistgrp',
+                                                  cmdopts=cmdopts,
+                                                  cmdargs=[self.name])
+            if result == '':
+                self.changed = True
+                self.log("succeeded to start the remote copy group %s", self.name)
+            elif 'message' in result:
+                self.changed = True
+                self.log("start the remote copy group %s with result message %s", self.name, result['message'])
+            else:
+                msg = "Failed to start the remote copy group [%s]" % self.name
+                self.module.fail_json(msg=msg)
+        else:
+            result = self.restapi.svc_run_command(cmd='startrcrelationship',
+                                                  cmdopts=cmdopts,
+                                                  cmdargs=[self.name])
+            self.log("start the rcrelationship %s with result %s", self.name, result)
+            if result == '':
+                self.changed = True
+                self.log("succeeded to start the remote copy %s", self.name)
+            elif 'message' in result:
+                self.changed = True
+                self.log("start the rcrelationship %s with result message %s", self.name, result['message'])
+            else:
+                msg = "Failed to start the rcrelationship [%s]" % self.name
+                self.module.fail_json(msg=msg)
+
+    def stop(self):
+        """
+        Stops the copy process for a Metro Mirror or Global Mirror stand-alone
+        relationship.
+        """
+        cmdopts = {}
+        if self.access:
+            cmdopts['access'] = self.access
+        if self.isgroup:
+            result = self.restapi.svc_run_command(cmd='stoprcconsistgrp',
+                                                  cmdopts=cmdopts,
+                                                  cmdargs=[self.name])
+            self.log("stop the remote copy group %s with result %s", self.name, result)
+            if result == '':
+                self.changed = True
+                self.log("succeeded to stop the remote copy group %s", self.name)
+            elif 'message' in result:
+                self.changed = True
+                self.log("stop the remote copy group %s with result message %s", self.name, result['message'])
+            else:
+                msg = "Failed to stop the rcrelationship [%s]" % self.name
+                self.module.fail_json(msg=msg)
+        else:
+            result = self.restapi.svc_run_command(cmd='stoprcrelationship', cmdopts=cmdopts, cmdargs=[self.name])
+            self.log("stop the rcrelationship %s with result %s", self.name, result)
+            if result == '':
+                self.changed = True
+                self.log("succeeded to stop the remote copy %s", self.name)
+            elif 'message' in result:
+                self.changed = True
+                self.log("stop the rcrelationship %s with result message %s", self.name, result['message'])
+            else:
+                msg = "Failed to stop the rcrelationship [%s]" % self.name
+                self.module.fail_json(msg=msg)
+
+    def ishyperswap(self, data):
+        if data:
+            if data['copy_type'] == "activeactive":
+                return True
+
+    def apply(self):
+        changed = False
+        msg = None
+        ishyperswap = False
+        self.log("self state is %s", self.state)
+        if not self.isgroup:
+            rcrelationship_data = self.existing_rc()
+        else:
+            rcrelationship_data = self.existing_rccg()
+        if rcrelationship_data:
+            ishyperswap = self.ishyperswap(rcrelationship_data)
+            if (ishyperswap):
+                self.module.fail_json(msg="active-active relationships are not supported")
+        if self.module.check_mode:
+            self.log('skipping changes due to check mode.')
+            msg = 'skipping changes due to check mode.'
+        else:
+            if self.state == 'started':
+                self.start()
+                changed = True
+                if not self.isgroup:
+                    msg = "remote copy [%s] has been started." % self.name
+                else:
+                    msg = "remote copy group [%s] has been started." % self.name
+            elif self.state == 'stopped':
+                self.stop()
+                changed = True
+                if not self.isgroup:
+                    msg = "remote copy [%s] has been stopped." % self.name
+                else:
+                    msg = "remote copy group [%s] has been stopped." % self.name
+            else:
+                msg = "Invalid %s state. Supported states are 'started' and 'stopped'" % self.state
+
+        self.module.exit_json(msg=msg, changed=changed)
+
+
+def main():
+    v = IBMSVCStartStopReplication()
+    try:
+        v.apply()
+    except Exception as e:
+        v.log("Exception in apply(): \n%s", format_exc())
+        v.module.fail_json(msg="Module failed. Error [%s]." % to_native(e))
+
+
+if __name__ == '__main__':
+    main()
---
+++ ibm_svc_vol_map.py
@@ -0,0 +1,310 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020 IBM CORPORATION
+# Author(s): Peng Wang <wangpww@cn.ibm.com>
+#            Sreshtant Bohidar <sreshtant.bohidar@ibm.com>
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'metadata_version': '1.1'}
+
+DOCUMENTATION = '''
+---
+module: ibm_svc_vol_map
+short_description: This module manages volume mapping on IBM Spectrum
+                   Virtualize Family storage systems.
+description:
+  - Ansible interface to manage volume mapping commands
+    'mkvdiskhostmap' and 'rmvdiskhostmap' commands.
+version_added: "2.10.0"
+options:
+  volname:
+    description:
+      - Specifies the volume name for host mapping.
+    required: true
+    type: str
+  host:
+    description:
+      - Specifies the host name for host mapping.
+    required: true
+    type: str
+  state:
+    description:
+      - Creates (C(present)) or removes (C(absent)) a volume mapping.
+    choices: [ absent, present ]
+    required: true
+    type: str
+  clustername:
+    description:
+      - The hostname or management IP of the
+        Spectrum Virtualize storage system.
+    type: str
+    required: true
+  domain:
+    description:
+      - Domain for the Spectrum Virtualize storage system.
+    type: str
+  username:
+    description:
+      - REST API username for the Spectrum Virtualize storage system.
+    required: true
+    type: str
+  password:
+    description:
+      - REST API password for the Spectrum Virtualize storage system.
+    required: true
+    type: str
+  log_path:
+    description:
+    - Path of debug log file.
+    type: str
+  validate_certs:
+    description:
+    - Validates certification.
+    default: false
+    type: bool
+author:
+    - Peng Wang(@wangpww)
+'''
+
+EXAMPLES = '''
+- name: Using Spectrum Virtualize collection to map a volume to a host
+  hosts: localhost
+  collections:
+    - ibm.spectrum_virtualize
+  gather_facts: no
+  connection: local
+  tasks:
+    - name: Map a volume to a host
+      ibm_svc_vol_map:
+        clustername: "{{clustername}}"
+        domain: "{{domain}}"
+        username: "{{username}}"
+        password: "{{password}}"
+        log_path: /tmp/playbook.debug
+        volname: volume0
+        host: host4test
+        state: present
+
+- name: Using Spectrum Virtualize collection to unmap a volume from a host
+  hosts: localhost
+  collections:
+    - ibm.spectrum_virtualize
+  gather_facts: no
+  connection: local
+  tasks:
+    - name: Unmap a volume from a host
+      ibm_svc_vol_map:
+        clustername: "{{clustername}}"
+        domain: "{{domain}}"
+        username: "{{username}}"
+        password: "{{password}}"
+        log_path: /tmp/playbook.debug
+        volname: volume0
+        host: host4test
+        state: absent
+'''
+RETURN = '''
+'''
+
+from traceback import format_exc
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.ibm.spectrum_virtualize.plugins.module_utils.ibm_svc_utils import IBMSVCRestApi, svc_argument_spec, get_logger
+from ansible.module_utils._text import to_native
+
+
+class IBMSVCvdiskhostmap(object):
+    def __init__(self):
+        argument_spec = svc_argument_spec()
+
+        argument_spec.update(
+            dict(
+                volname=dict(type='str', required=True),
+                host=dict(type='str', required=True),
+                state=dict(type='str', required=True, choices=['absent',
+                                                               'present']),
+            )
+        )
+
+        self.module = AnsibleModule(argument_spec=argument_spec,
+                                    supports_check_mode=True)
+
+        # logging setup
+        log_path = self.module.params['log_path']
+        log = get_logger(self.__class__.__name__, log_path)
+        self.log = log.info
+
+        # Required
+        self.volname = self.module.params['volname']
+        self.host = self.module.params['host']
+        self.state = self.module.params['state']
+
+        # Optional
+
+        self.restapi = IBMSVCRestApi(
+            module=self.module,
+            clustername=self.module.params['clustername'],
+            domain=self.module.params['domain'],
+            username=self.module.params['username'],
+            password=self.module.params['password'],
+            validate_certs=self.module.params['validate_certs'],
+            log_path=log_path
+        )
+
+    def get_existing_vdiskhostmap(self):
+        merged_result = []
+
+        data = self.restapi.svc_obj_info(cmd='lsvdiskhostmap', cmdopts=None,
+                                         cmdargs=[self.volname])
+
+        if isinstance(data, list):
+            for d in data:
+                merged_result.append(d)
+        elif data:
+            merged_result = [data]
+
+        return merged_result
+
+    # TBD: Implement a more generic way to check for properties to modify.
+    def vdiskhostmap_probe(self, mdata):
+        props = []
+        self.log("vdiskhostmap_probe props='%s'", mdata)
+        # TBD: The parameter is easytier but the view has easy_tier label.
+        mapping_exist = False
+        for data in mdata:
+            if (self.host == data['host_name']) and (self.volname == data['name']):
+                mapping_exist = True
+
+        if not mapping_exist:
+            props += ["map"]
+
+        if props is []:
+            props = None
+
+        self.log("vdiskhostmap_probe props='%s'", props)
+        return props
+
+    def vdiskhostmap_create(self):
+        if self.module.check_mode:
+            self.changed = True
+            return
+
+        if not self.volname:
+            self.module.fail_json(msg="You must pass in "
+                                      "volname to the module.")
+        if not self.host:
+            self.module.fail_json(msg="You must pass in host "
+                                      "name to the module.")
+
+        self.log("creating vdiskhostmap '%s' '%s'", self.volname, self.host)
+
+        # Make command
+        cmd = 'mkvdiskhostmap'
+        cmdopts = {'force': True}
+        cmdopts['host'] = self.host
+        cmdargs = [self.volname]
+
+        self.log("creating vdiskhostmap command %s opts %s args %s",
+                 cmd, cmdopts, cmdargs)
+
+        # Run command
+        result = self.restapi.svc_run_command(cmd, cmdopts, cmdargs)
+        self.log("create vdiskhostmap result %s", result)
+
+        if 'message' in result:
+            self.changed = True
+            self.log("create vdiskhostmap result message %s",
+                     result['message'])
+        else:
+            self.module.fail_json(msg="Failed to create vdiskhostmap.")
+
+    def vdiskhostmap_update(self, modify):
+        # vdiskhostmap_update() doesn't actually update anything as of now, it is here just as a placeholder.
+        # update the vdiskhostmap
+        self.log("updating vdiskhostmap")
+
+        if 'host_name' in modify:
+            self.log("host name is changed.")
+
+        if 'volname' in modify:
+            self.log("vol name is changed.")
+
+        self.changed = True
+
+    def vdiskhostmap_delete(self):
+        self.log("deleting vdiskhostmap '%s'", self.volname)
+
+        cmd = 'rmvdiskhostmap'
+        cmdopts = {}
+        cmdopts['host'] = self.host
+        cmdargs = [self.volname]
+
+        self.restapi.svc_run_command(cmd, cmdopts, cmdargs)
+
+        # Any error will have been raised in svc_run_command
+        # chmvdisk does not output anything when successful.
+        self.changed = True
+
+    def apply(self):
+        changed = False
+        msg = None
+        modify = []
+
+        vdiskhostmap_data = self.get_existing_vdiskhostmap()
+        self.log("volume mapping data is : '%s'", vdiskhostmap_data)
+
+        if vdiskhostmap_data:
+            if self.state == 'absent':
+                self.log("vdiskhostmap exists, "
+                         "and requested state is 'absent'")
+                changed = True
+            elif self.state == 'present':
+                probe_data = self.vdiskhostmap_probe(vdiskhostmap_data)
+                if probe_data:
+                    self.log("vdiskhostmap does not exist, but requested state is 'present'")
+                    changed = True
+        else:
+            if self.state == 'present':
+                self.log("vdiskhostmap does not exist, "
+                         "but requested state is 'present'")
+                changed = True
+
+        if changed:
+            if self.module.check_mode:
+                self.log('skipping changes due to check mode')
+                msg = 'skipping changes due to check mode'
+            else:
+                if self.state == 'present':
+                    self.vdiskhostmap_create()
+                    msg = "vdiskhostmap %s %s has been created." % (
+                        self.volname, self.host)
+                elif self.state == 'absent':
+                    self.vdiskhostmap_delete()
+                    msg = "vdiskhostmap [%s] has been deleted." % self.volname
+        else:
+            self.log("exiting with no changes")
+            if self.state == 'absent':
+                msg = "vdiskhostmap [%s] did not exist." % self.volname
+            else:
+                msg = "vdiskhostmap [%s] already exists." % self.volname
+
+        self.module.exit_json(msg=msg, changed=changed)
+
+
+def main():
+    v = IBMSVCvdiskhostmap()
+    try:
+        v.apply()
+    except Exception as e:
+        v.log("Exception in apply(): \n%s", format_exc())
+        v.module.fail_json(msg="Module failed. Error [%s]." % to_native(e))
+
+
+if __name__ == '__main__':
+    main()
